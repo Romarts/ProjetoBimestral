c0f263322d8bd8dcbc4e68ff636ad047aea7562c
Romarts <gabrielromatarta@gmail.com>
2025-09-14 23:07:14 -0300

    Terceiro commit

--- TECHFORGE/TECH FORGE/Exercicios/ExercicioArrays.js
+++
@@ -1,9 +0,0 @@
-let aluno = {
-    nome: "Roberto Silva",
-    matricula: "2025XPTO",
-    notas:[8.5, 7.0, 9.2, 6.5]
-};
-
-console.log(aluno.notas[1]);
-
-console.log("A primeira nota de "+aluno.nome+" foi "+aluno.notas[0]);
--- TECHFORGE/TECH FORGE/Exercicios/TECHFORGE.js
+++
@@ -1,6 +0,0 @@
-const prompt = require("prompt-sync")();
-
-var i1 = (prompt("Digite o seu nome: "));
-var i2 = (prompt("Digite o seu endereço: "))
-var i3 = (prompt("Digite o seu telefone: "))
-console.log("Nome: "+i1+ " Endereço: "+i2+" Telefone: "+i3)
